de88a36f0a8f674cbdf6287f6d7dbf998f13c3fd
Rhutik Chaudhari <rhutikchaudhari662@gmail.com>
2024-06-20 10:54:33 +0530

    rhutik: store updated with news data

--- src/redux/actions/action.ts
+++ src/redux/actions/action.ts
@@ -1,10 +1,15 @@
 
-export const nameUpdate = (name:string)=>({
-    type:'NAME_UPDATE',
-    payload:name
+export const newsUpdate = (news:any)=>({
+    type:'NEWS_UPDATE',
+    payload:news
 })
 
 export const currentCategoryUpdate = (category:string)=>({
     type:'CATEGORY_UPDATE',
     payload:category
+})
+
+export const filteredDataUpdate = (data:any)=>({
+    type:'FILTERED_DATA_UPDATE',
+    payload: data
 })
--- src/redux/init.ts
+++ src/redux/init.ts
@@ -1,4 +1,5 @@
 export const init:Object = {
-    name:'',
-    currentCategory:''
+    news:[],
+    currentCategory:'',
+    fdata :[]
 }
